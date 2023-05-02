8a9d98657aef4139d23afd8d875ea9c56da0aa9b
1abhi6 <1abhigup6@gmail.com>
2023-05-02 20:41:39 +0530

    Added login and register feature with encryption

--- app.py
+++ app.py
@@ -1,10 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template,request
+from database import Database
+
 
 app = Flask(__name__)
+db = Database()
+
+
 
 @app.route('/')
 def index():
-    return 'Hello'
+    return render_template('login.html')
 
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/perform_registration',methods=['POST'])
+def perform_registration():
+    name = request.form.get('name')
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    
+    response = db.insert(name,email,password)
+    
+    if response:
+        return "Bhai tu login hogya"
+    return 'Abey tera email already register h login kr'
+
+@app.route('/perform_login',methods=['POST'])
+def perform_login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+
+    response = db.fetch(email,password)
+    
+    if response:
+        return 'Beta tu login hogya'
+    return render_template('register.html')
 
 app.run(debug=True)
---
+++ database.py
@@ -0,0 +1,23 @@
+import json
+
+class Database:
+
+    def insert(self, name, email, password):
+
+        with open('users.json', 'r', encoding='utf8') as read_file:
+            users = json.load(read_file)
+
+            if email in users:
+                return False
+            users[email] = [name, password]
+
+        with open('users.json', 'w', encoding='utf8') as write_file:
+            json.dump(users, write_file, indent=4)
+            return True
+
+    def fetch(self, email, password):
+        with open('users.json', 'r', encoding='utf8') as read_file:
+            users = json.load(read_file)
+            if email in users and users[email][1] == password:
+                return True
+            return False
