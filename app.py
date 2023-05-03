8d724f747b47b34523fad4fe092473a57e8f5f40
1abhi6 <1abhigup6@gmail.com>
2023-05-03 18:39:58 +0530

    Added Login and register feature without password encryption

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template, request
 from database import Database
 
 
 app = Flask(__name__)
 db = Database()
-
 
 
 @app.route('/')
@@ -17,29 +16,29 @@ def register():
     return render_template('register.html')
 
 
-@app.route('/perform_registration',methods=['POST'])
+@app.route('/perform_registration', methods=['POST'])
 def perform_registration():
     name = request.form.get('name')
     email = request.form.get('email')
     password = request.form.get('password')
 
-    
-    response = db.insert(name,email,password)
-    
-    if response:
-        return "Bhai tu login hogya"
-    return 'Abey tera email already register h login kr'
+    response = db.insert(name, email, password)
 
-@app.route('/perform_login',methods=['POST'])
+    if response:
+        return render_template('login.html', register_success=True)
+    return render_template('register.html', register_failed=True)
+
+
+@app.route('/perform_login', methods=['POST'])
 def perform_login():
     email = request.form.get('email')
     password = request.form.get('password')
 
+    response = db.fetch(email, password)
 
-    response = db.fetch(email,password)
-    
     if response:
         return 'Beta tu login hogya'
-    return render_template('register.html')
+    return render_template('login.html', login_failed=True)
+
 
 app.run(debug=True)
